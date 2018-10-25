cdc66ad60fb78dee0e160fa239d2990301dd5c14
huwenhao <1398744100@qq.com>
2018-10-25 21:49:02 +0800

    apriori

--- FrequentItemsetsMiningMethod/Apriori.py
+++ FrequentItemsetsMiningMethod/Apriori.py
@@ -8,23 +8,6 @@ np.set_printoptions(threshold=np.inf)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
-# data_set: list
-# frequent_itemset: list['set','set']
-# candidata_itemset:
-def gen_candidate_itemset(data_set, frequent_itemset):
-    candidate_itemset = []
-    candidate_itemset_count = {}
-    item = set()
-    for i in data_set:
-        for j in i:
-            if j not in item:
-                item.add(j)
-                candidate_itemset.update({j: 0})
-
-        print(candidate_itemset)
-    return candidate_itemset, candidate_itemset_count
-
-
 # 找出Cki的所有长度为k-1的子集，判断是否在Lk中,不在Lk中则需要pruning，返回True
 def is_pruning(Cki, Lk):
     C = frozenset(Cki)
@@ -38,6 +21,7 @@ def is_pruning(Cki, Lk):
     return False
 
 
+# 迭代求解C
 def iteration(Ck, frequent_itemset, min_sup, data_set):
     # 删除
     for i in frozenset(Ck.keys()):
@@ -53,7 +37,6 @@ def iteration(Ck, frequent_itemset, min_sup, data_set):
     Ck.clear()
 
     len_Lk = len(Lk)
-    print(Lk)
     for i in range(len_Lk):
         for j in range(1,len_Lk):
             L1 = list(Lk[i])
@@ -73,14 +56,20 @@ def iteration(Ck, frequent_itemset, min_sup, data_set):
         for j in data_set:
             if set(item) & set(j) == set(item):
                 Ck[i] += 1
-
     return Ck
+
+
+def print_frequent_itemset(frequent_itemset):
+    print('frequent_itemset', ' '*20, 'support')
+    print('-'*60)
+    for i in frequent_itemset:
+        print('%-38s%.3f' % (list(i), round(frequent_itemset[i], 3)))
+    return None
 
 
 # 项（item）：可能包含多个字符串，结构是set，存入项集时转换为frozenset
 # frequent_itemset: 总的频繁项集，频繁项的集合及对应支持度，结构是字典，key的结构为set->frozenset
 # candidate_itemset： 候选项集，候选项的集合及对应支持度， 结构是字典， key的结构为set->frozenset
-# candidate_itemset_count = {}: 包含支持度的项集，由于可变集合无法作为字典的key，需要使用frozenset结构
 def apriori(data_set, min_sup=2):
     frequent_itemset = {}
     candidate_itemset = {}
@@ -98,19 +87,16 @@ def apriori(data_set, min_sup=2):
     while candidate_itemset:
         candidate_itemset = iteration(candidate_itemset,frequent_itemset, min_sup, data_set)
         k += 1
-        print('迭代次数：', k)
-    print(frequent_itemset)
+        print('\r', '迭代次数：%d' % k, end=' ')
+    for i in frequent_itemset:
+        frequent_itemset[i] = frequent_itemset[i]/len(data_set)
+    print()
+    print_frequent_itemset(frequent_itemset)
     return frequent_itemset
 
 
-dataset = [['i1', 'i2', 'i5'], ['i2', 'i4'], ['i2', 'i3'], ['i1', 'i2', 'i4'], ['i1', 'i3'], ['i2', 'i3'],
-            ['i1', 'i3'], ['i1', 'i2', 'i3', 'i5'], ['i1', 'i2', 'i3']]
-
-
-a = [{'a', 'b', 'c'}, {'a', 'b', 'd'}, {'a', 'c', 'd'}, {'a', 'c', 'e'}, {'b', 'c', 'd'}]
-b = {'a', 'c', 'd', 'e'}
-c = {'a': 1, 'b': 1, 'c': 1, 'd': 1}
-d = {'e': 2, frozenset({'a','b'}): 3}
-#c.update(d)
-#print(c)
+dataset = [['牛奶', '面包', '尿布'], ['面包', '啤酒'], ['面包', '黄油'], ['牛奶', '面包', '啤酒'], ['牛奶', '黄油'], ['面包', '黄油'],
+           ['牛奶', '黄油'], ['牛奶', '面包', '黄油', '尿布'], ['牛奶', '面包', '黄油']]
 apriori(dataset)
+
+
